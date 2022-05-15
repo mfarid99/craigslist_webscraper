73af867f2359ccf9931e887b64ce4e8abbe7d87e
Moe <moe@triblio.com>
2022-05-15 17:01:08 -0400

    job description scrapped

--- index.js
+++ index.js
@@ -48,6 +48,10 @@ async function scrapeJobDescription(listings, page){
 for(i = 0; i < listings.length; i++){
     await page.goto(listings[i].url);
     const html = await page.content();
+    const $ = cheerio.load(html)
+    const jobDescription = $('#postingbody').text();
+    listings[i].jobDescription = jobDescription;
+    console.log(listings[i].jobDescription)
     await sleep(3000);
 }
 }
